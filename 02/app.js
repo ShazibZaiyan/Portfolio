2cde171b30ea895ec8f05e985c5a1e2c7ec3cb35
Shazib Zaiyan <117989356+ShazibZaiyan@users.noreply.github.com>
2022-12-19 14:33:52 +0530

    Add files via upload
    Tech stack: HTML, CSS, and JavaScript

---
+++ 02/app.js
@@ -0,0 +1,25 @@
+const humburger = document.querySelector('.header .nav-bar .nav-list .humburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+
+humburger.addEventListener('click', () => {
+	humburger.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	if (scroll_position > 250) {
+		header.style.backgroundColor = '#29323c';
+	} else {
+		header.style.backgroundColor = 'transparent';
+	}
+});
+
+menu_item.forEach((item) => {
+	item.addEventListener('click', () => {
+		humburger.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
+});
